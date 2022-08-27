67b98c112d30e08b4a8504dde46203eb2498effe
FlavioLionelRita <flaviolrital@hotmail.com>
2022-08-27 03:41:17 +0200

    update js-expression version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,39 +1,43 @@
 #!/usr/bin/env node
-import { expressions as exp } from 'js-expressions'
-import { Helper } from './helper'
+import { expressions as exp, Helper } from 'js-expressions'
 import path from 'path'
 const yaml = require('js-yaml')
-
-const readFile = async (file:string):Promise<any> => {
-	const extension = path.extname(file)
-	if (!['.json', '.yaml', 'yml'].includes(extension)) {
-		throw Error(`extension ${extension} not supported `)
-	}
-	const content = await Helper.readFile(file)
-	if (!content) {
-		throw Error(`can not read file ${file}`)
-	}
-	if (extension === 'json') {
-		return Helper.tryParse(content)
-	} else {
-		return yaml.load(content)
-	}
-}
 
 export async function run () {
 	try {
 		if (process.argv.length !== 4) {
-			console.error(`Error: 2 arguments were expected and 3 came ${process.argv.length - 2}`)
+			throw new Error(`Error: 2 arguments were expected and 3 came ${process.argv.length - 2}`)
 		}
 		const expression = process.argv[2]
 		const source = process.argv[3].trim()
-
-		const data = (source.startsWith('[') || source.startsWith('{')) ? Helper.tryParse(source) : await readFile(source)
+		let data:any = {}
+		let extension = ''
+		if ((source.startsWith('[') || source.startsWith('{'))) {
+			data = Helper.tryParse(source)
+		} else {
+			extension = path.extname(source)
+			if (!['.json', '.yaml', 'yml'].includes(extension)) {
+				throw Error(`extension ${extension} not supported `)
+			}
+			const content = await Helper.readFile(source)
+			if (!content) {
+				throw Error(`can not read file ${source}`)
+			}
+			if (extension === 'json') {
+				data = Helper.tryParse(content)
+			} else {
+				data = yaml.load(content)
+			}
+		}
 		if (data === null || data === undefined) {
 			throw Error(`can not parse content of ${source}`)
 		}
 		const result = exp.eval(expression, { _: data })
-		console.log(result)
+		if (extension && ['.yaml', 'yml'].includes(extension)) {
+			console.log(yaml.dump(result))
+		} else {
+			console.log(JSON.stringify(result, null, 2))
+		}
 	} catch (error:any) {
 		console.error(error.stack)
 	}
